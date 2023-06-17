2cd76eefbaf1c0cda3be6d55c9352689cb91f062
plisken1509 <solorzanopablo81@gmail.com>
2023-06-17 11:02:17 -0500

    web scraping
    scraping listo

---
+++ main.py
@@ -0,0 +1,41 @@
+import time
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+chrome_driver = webdriver.Chrome()
+
+# bot = TelegramBot()
+# db = Mongo()
+
+
+chrome_driver.get("https://www.wikipedia.org/")
+print(chrome_driver.title)
+
+topics = [
+    "Alhambra",
+    "Generalife",
+    "gonzalez suarez"
+]
+
+for t in topics:
+    search_box = chrome_driver.find_element(By.ID, "searchInput")
+    search_box.send_keys(t)
+    search_button = chrome_driver.find_element(By.CSS_SELECTOR, '#search-form > fieldset > button > i')
+    search_button.click()
+
+    content = chrome_driver.find_element(By.ID, "mw-content-text")
+    print(content.text)
+
+    # enviar por telegram
+    # bot.send(content.text, chat_id)
+    # db.insert_wikipedia_text({
+    #   "topic": content.text
+    # }
+
+    time.sleep(2)
+    chrome_driver.get("https://www.wikipedia.org/")
+
+
+print("fin")
+chrome_driver.close()
